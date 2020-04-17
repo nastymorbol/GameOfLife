7c8c866ede052ca483a333a2655ab256962267e3
Schulze, Steve <S.Schulze@deos-ag.com>
2020-04-17 22:18:13 +0200

    Run Main Gameloop

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 
 namespace GameOfLife
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            using (var game = new Game1())
+                game.Run();
         }
     }
 }
